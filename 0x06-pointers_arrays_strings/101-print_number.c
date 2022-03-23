ef01ea07ea5d0db008107054ff8bde1285f09427
fraoulmas <fraoulmas@gmail.com>
2022-03-23 19:25:00 +0300

    fixed

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,31 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
 /**
- *print_number - prints an integer.
- *only using the putchar function.
- *noarrays and pointers.
- *@n: integer to be printed.
- *
- *Return: void.
+ * print_number - that prints an integer
+ * @n: number to string
+ * Returnn: nothing
  */
+
 void print_number(int n)
 {
-unsigned int num;
-/*check if number is negative*/
-num = n;
-if (n < 0)
-{
-_putchar(45);
-num = -n;
-}
-/* print number by recursion*/
-if (num / 10)
-{
-print_number(num / 10);
-}
-_putchar((num % 10) + '0');
+	int pot_10 = 1, sign = 1, tmp = n;
+
+	while (tmp / 10)
+	{
+		pot_10 *= 10;
+		tmp /= 10;
+	}
+
+	if (tmp < 0)
+	{
+		sign *= -1;
+		_putchar('-');
+	}
+	while (pot_10 > 0)
+	{
+		tmp = n / pot_10;
+		_putchar((tmp * sign) + '0');
+		n = n - (tmp * pot_10);
+		pot_10 /= 10;
+	}
 }
