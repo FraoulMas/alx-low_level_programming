4a4bf3e6a0e81401935f41f156053fdcae9d5ec5
fraoulmas <fraoulmas@gmail.com>
2022-04-07 22:41:39 +0300

    checking for error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,15 +2,14 @@
 
 /**
  * malloc_checked - allocates memory using malloc
- * @b: bytes allocate
- * Return: pointer
+ * @b: number of bytes to be malloced
+ * Return: returns a void pointer
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
-
-	ptr = malloc(b);
-	if (ptr == NULL)
-		exit(98);
-	return (ptr);
+void *ptr;
+ptr = malloc(b);
+if (!ptr)
+exit(98);
+return (ptr);
 }
